e093bf62f8c800533e17c27578ca4ff60faf9bf7
TimaGitHub <70072941+TimaGitHub@users.noreply.github.com>
2026-04-09 23:58:58 +0300

    Comment out CUDA_VISIBLE_DEVICES setting
    Comment out the setting of CUDA_VISIBLE_DEVICES environment variable.

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ def organize_files_by_copying():
 
 if __name__ == "__main__":
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     results_folder = "./results"
     train_num_workers = 0
     with open(os.path.join(results_folder, "params.pkl"), "rb") as f:
