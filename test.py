ef3df9c3cc18157b7e4798c49dc4510eab9bdc9c
TimaGitHub <70072941+TimaGitHub@users.noreply.github.com>
2026-04-09 21:57:22 +0300

    Change sampling_timesteps and update file paths

--- test.py
+++ test.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
 
     model = Unet(**params["unet_dict"])
 
-    # params["diffusion_dict"]["sampling_timesteps"] = 10
+    params["diffusion_dict"]["sampling_timesteps"] = 2
 
     diffusion = GaussianDiffusion(model, **params["diffusion_dict"])
 
     trainer = Trainer(
         diffusion,
-        "../chat_test_data/0614-kimi/image",
-        "../chat_test_data/0614-kimi/mask",
-        "../chat_test_data/0614-kimi/text",
+        "./chat_test_data/0614-kimi/image",
+        "./chat_test_data/0614-kimi/mask",
+        "./chat_test_data/0614-kimi/text",
         **params["trainer_dict"],
         results_folder=results_folder,
         train_num_workers=train_num_workers,
